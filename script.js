e93cf0db5e65f8f6b5334ceb1000c9276dce2a31
AudreyKangah <audreykan68@gmail.com>
2025-03-14 10:00:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        // sticky navbar on scroll script
+        // barre de navigation collante sur script de défilement
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
         
-        // scroll-up button show/hide script
+        // bouton de défilement vers le haut afficher/masquer le script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
             $('.scroll-up-btn').removeClass("show");
         }
     });
-    // slide-up script
+    // script coulissant
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
+        // suppression du défilement fluide lors du clic du bouton coulissant
         $('html').css("scrollBehavior", "auto");
     });
     $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
+        // appliquer à nouveau un défilement fluide sur les éléments de menu en cliquant
         $('html').css("scrollBehavior", "smooth");
     });
-    // toggle menu/navbar script
+    // script de basculement du menu/barre de navigation
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-    // typing text animation script
+    // script d'animation de saisie de texte
     var typed = new Typed(".typing", {
-        strings: ["Data Analyst", "Analyste de données", "Blogger", "Designer", "Freelancer"],
+        strings: ["Data Analyst", "Analyste de données"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Data Analyst", "Analyste de données", "Blogger", "Designer", "Freelancer"],
+        strings: ["Data Analyst", "Analyste de données"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
-    // owl carousel script
+    // script de carrousel de hiboux
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
